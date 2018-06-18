79d90cdbc2c674214ecd57b7c85a9485ff42f40c
NikolayPautov <npa@sitecore.net>
2018-06-18 16:40:35 +0300

    Initial patch

---
+++ src/Sitecore.Support.13144/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.13144")]
+[assembly: AssemblyProduct("Sitecore.Support.13144")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.13144/XA/Foundation/Multisite/EventHandlers/VirtualMediaFolderValidator.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.DependencyInjection;
+using Sitecore.Events;
+using Sitecore.XA.Foundation.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.XA.Foundation.Multisite;
+
+namespace Sitecore.Support.XA.Foundation.Multisite.EventHandlers
+{
+  public class VirtualMediaFolderValidator: Sitecore.XA.Foundation.Multisite.EventHandlers.VirtualMediaFolderValidator
+  {
+    new protected void OnItemMoving(object obj, EventArgs args)
+    {
+      
+      Database contentDatabase = ServiceProviderServiceExtensions.GetService<IContext>(ServiceLocator.ServiceProvider).ContentDatabase;
+      if(contentDatabase!=null)
+      {
+        base.OnItemMoving(obj, args);
+      }
+    }
+  }
+}
